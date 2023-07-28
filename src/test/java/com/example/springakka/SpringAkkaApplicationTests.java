3f1071b2df6a51ccb74cc77d4ed4d43d9305a099
Alessandro Novi <alessandro.novi@atscom.it>
2023-07-28 17:52:48 +0200

    init spring web
    (cherry picked from commit 5ed87515a6eab2718fc44229797a461d585a2c96)

---
+++ src/test/java/com/example/springakka/SpringAkkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springakka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAkkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
