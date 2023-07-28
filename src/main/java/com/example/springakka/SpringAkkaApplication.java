5ed87515a6eab2718fc44229797a461d585a2c96
Alessandro Novi <alessandro.novi@atscom.it>
2023-07-28 17:52:48 +0200

    init spring web

---
+++ src/main/java/com/example/springakka/SpringAkkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springakka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAkkaApplication.class, args);
+	}
+
+}
